5a8b557325cc96fd20a21ad21893d93db4e0156f
Minniesse <pkongkae@cmkl.ac.th>
2023-10-22 10:44:04 +0700

    Connect to the pages when click button

--- SETL.py
+++ SETL.py
@@ -1,11 +1,120 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QFont
 from ui.Main_interface_ui import Ui_MainWindow
+
+#import the pages_functions
+from page_functions.translate import Translate
+from page_functions.vocabulary import Vocabulary
+from page_functions.history import History
+from page_functions.favourite import Favourite
+
 
 class root(QMainWindow):
     def __init__(self):
         super(root, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        ## ======================
+        ## Adding all pages
+        ## ======================
+        self.home_btn = self.ui.pushButton_4
+        self.Vocabulary_button = self.ui.pushButton_3
+        self.History_button = self.ui.pushButton_2
+        self.Favourite_button = self.ui.pushButton
+        
+        ## ==================================================
+        ## create menu buttons (nav bar) and variables
+        ## ==================================================
+        self.nav_button_dict = {
+            self.home_btn: Translate,
+            self.Vocabulary_button: Vocabulary,
+            self.History_button: History,
+            self.Favourite_button: Favourite,
+        }
+
+        ## ===========================================
+        ## Show Translate page when the program opened
+        ## ===========================================
+        self.show_home_window()
+
+        ## Set fonts
+        font = QFont("Poppins", 12)
+        self.ui.tabWidget.setFont(font)
+
+        ## ===========================================
+        ## Connect the buttons to the functions
+        ## ===========================================
+        self.home_btn.clicked.connect(self.show_home_window)
+        self.Vocabulary_button.clicked.connect(self.show_selected_window)
+        self.History_button.clicked.connect(self.show_selected_window)
+        self.Favourite_button.clicked.connect(self.show_selected_window)
+
+
+    def show_home_window(self):
+        """
+        show the Translate (home) page
+        :return:
+        """
+        result = self.open_tab_flag(self.home_btn.text())
+        self.set_btn_checked(self.home_btn)
+
+        if result[0]:
+            self.ui.tabWidget.setCurrentIndex(result[1])
+        else:
+            tab_title = self.home_btn.text()
+            curIndex = self.ui.tabWidget.addTab(Translate(), tab_title)
+            self.ui.tabWidget.setCurrentIndex(curIndex)
+            self.ui.tabWidget.setVisible(True)
+
+    
+    def set_btn_checked(self, btn):
+        """
+        Set the button status
+        :param btn: button object
+        :return:
+        """
+        for button in self.nav_button_dict.keys():
+            if button != btn:
+                button.setChecked(False)
+            else:
+                button.setChecked(True)
+    
+    def show_selected_window(self):
+        """
+        function for showing the selected page
+        :return:
+        """
+        button = self.sender()
+        result = self.open_tab_flag(button.text())
+        self.set_btn_checked(button)
+
+        if result[0]:
+            self.ui.tabWidget.setCurrentIndex(result[1])
+        else:
+            tab_title = button.text()
+            curIndex = self.ui.tabWidget.addTab(self.nav_button_dict[button](), tab_title)
+            self.ui.tabWidget.setCurrentIndex(curIndex)
+            self.ui.tabWidget.setVisible(True)
+
+
+    def open_tab_flag(self, btn_text):
+        """
+        When the home page open the button should be selected
+        :param btn_text: button text
+        :return: bool and index
+        """
+        open_tab_count = self.ui.tabWidget.count()
+
+        for i in range(open_tab_count):
+            tab_title = self.ui.tabWidget.tabText(i)
+            if tab_title == btn_text:
+                return True, i
+            else:
+                continue
+        return False,
+
+
 
 if __name__ == '__main__':
     import sys
--- ui/Main_interface_ui.py
+++ ui/Main_interface_ui.py
@@ -55,8 +55,13 @@ class Ui_MainWindow(object):
         self.label.setMinimumSize(QtCore.QSize(0, 100))
         self.label.setMaximumSize(QtCore.QSize(16777215, 16777215))
         font = QtGui.QFont()
+        font.setFamily("Poppins")
         font.setPointSize(25)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
         self.label.setFont(font)
+        self.label.setStyleSheet("font: 25pt \"Poppins\";")
         self.label.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
         self.label.setObjectName("label")
         self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label)
@@ -69,6 +74,7 @@ class Ui_MainWindow(object):
         self.frame_3.setMinimumSize(QtCore.QSize(150, 220))
         self.frame_3.setMaximumSize(QtCore.QSize(300, 16777215))
         self.frame_3.setStyleSheet("#widget QPushButton {\n"
+"    font: 17pt \"Poppins\";\n"
 "    display: flex;\n"
 "    text-align: left;\n"
 "    padding: 0px 0px 0px 20px;\n"
@@ -95,47 +101,71 @@ class Ui_MainWindow(object):
         self.pushButton_4 = QtWidgets.QPushButton(self.frame_3)
         self.pushButton_4.setMinimumSize(QtCore.QSize(0, 90))
         font = QtGui.QFont()
+        font.setFamily("Poppins")
         font.setPointSize(17)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
         self.pushButton_4.setFont(font)
+        self.pushButton_4.setFocusPolicy(QtCore.Qt.NoFocus)
         self.pushButton_4.setStyleSheet("")
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(":/icons/icons/Translate_logo.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.pushButton_4.setIcon(icon)
         self.pushButton_4.setIconSize(QtCore.QSize(40, 40))
+        self.pushButton_4.setCheckable(True)
         self.pushButton_4.setObjectName("pushButton_4")
         self.verticalLayout_3.addWidget(self.pushButton_4)
         self.pushButton_3 = QtWidgets.QPushButton(self.frame_3)
         self.pushButton_3.setMinimumSize(QtCore.QSize(0, 90))
         font = QtGui.QFont()
+        font.setFamily("Poppins")
         font.setPointSize(17)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
         self.pushButton_3.setFont(font)
+        self.pushButton_3.setFocusPolicy(QtCore.Qt.NoFocus)
         self.pushButton_3.setStyleSheet("")
         icon1 = QtGui.QIcon()
         icon1.addPixmap(QtGui.QPixmap(":/icons/icons/Vocabulary.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.pushButton_3.setIcon(icon1)
         self.pushButton_3.setIconSize(QtCore.QSize(40, 40))
+        self.pushButton_3.setCheckable(True)
         self.pushButton_3.setObjectName("pushButton_3")
         self.verticalLayout_3.addWidget(self.pushButton_3)
         self.pushButton_2 = QtWidgets.QPushButton(self.frame_3)
         self.pushButton_2.setMinimumSize(QtCore.QSize(0, 90))
         font = QtGui.QFont()
+        font.setFamily("Poppins")
         font.setPointSize(17)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
         self.pushButton_2.setFont(font)
+        self.pushButton_2.setFocusPolicy(QtCore.Qt.NoFocus)
         icon2 = QtGui.QIcon()
         icon2.addPixmap(QtGui.QPixmap(":/icons/icons/history.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.pushButton_2.setIcon(icon2)
         self.pushButton_2.setIconSize(QtCore.QSize(40, 40))
+        self.pushButton_2.setCheckable(True)
         self.pushButton_2.setObjectName("pushButton_2")
         self.verticalLayout_3.addWidget(self.pushButton_2)
         self.pushButton = QtWidgets.QPushButton(self.frame_3)
         self.pushButton.setMinimumSize(QtCore.QSize(0, 90))
         font = QtGui.QFont()
+        font.setFamily("Poppins")
         font.setPointSize(17)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
         self.pushButton.setFont(font)
+        self.pushButton.setFocusPolicy(QtCore.Qt.NoFocus)
         icon3 = QtGui.QIcon()
         icon3.addPixmap(QtGui.QPixmap(":/icons/icons/love.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.pushButton.setIcon(icon3)
         self.pushButton.setIconSize(QtCore.QSize(40, 40))
+        self.pushButton.setCheckable(True)
         self.pushButton.setObjectName("pushButton")
         self.verticalLayout_3.addWidget(self.pushButton)
         self.formLayout.setWidget(1, QtWidgets.QFormLayout.SpanningRole, self.frame_3)
@@ -165,24 +195,68 @@ class Ui_MainWindow(object):
         self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame_2.setObjectName("frame_2")
         self.verticalLayout_2.addWidget(self.widget_4, 0, QtCore.Qt.AlignHCenter|QtCore.Qt.AlignBottom)
-        self.widget_2.raise_()
-        self.widget_4.raise_()
         self.verticalLayout.addWidget(self.widget_3, 0, QtCore.Qt.AlignLeft)
         self.horizontalLayout.addWidget(self.widget)
         self.widget_5 = QtWidgets.QWidget(self.centralwidget)
         self.widget_5.setObjectName("widget_5")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.widget_5)
+        self.horizontalLayout_2.setContentsMargins(-1, 0, -1, 0)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.tabWidget = QtWidgets.QTabWidget(self.widget_5)
+        self.tabWidget.setStyleSheet("#tabWidget\n"
+"{\n"
+"    background-color: #FFF9F1;\n"
+"}\n"
+"\n"
+"QTabBar::close-button\n"
+"{\n"
+"    margin-left: 3px;\n"
+"}\n"
+"\n"
+"QTabBar::close:-button:hover\n"
+"{\n"
+"}\n"
+"\n"
+"QTabWidget::pane {\n"
+"                border-top: 2px solid #FFF9F1;\n"
+"                background-color: #FFF9F1;\n"
+"                border-radius: 10px;\n"
+"}\n"
+"\n"
+"QTabBar::tab {\n"
+"                background-color: #FFF6E0;\n"
+"                color: #333333;\n"
+"                border: 2px solid #FBDA8A;\n"
+"                border-top-left-radius: 10px;\n"
+"                border-top-right-radius: 10px;\n"
+"                min-width: 8ex;\n"
+"                padding: 5px;\n"
+"             margin-right: 10px;\n"
+"            }\n"
+"\n"
+"QTabBar::tab:selected {\n"
+"                background-color: #FBDA8A;\n"
+"                color: #00000;\n"
+"            }")
+        self.tabWidget.setTabPosition(QtWidgets.QTabWidget.North)
+        self.tabWidget.setTabShape(QtWidgets.QTabWidget.Rounded)
+        self.tabWidget.setTabsClosable(False)
+        self.tabWidget.setMovable(True)
+        self.tabWidget.setObjectName("tabWidget")
+        self.horizontalLayout_2.addWidget(self.tabWidget)
         self.horizontalLayout.addWidget(self.widget_5)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
+        self.tabWidget.setCurrentIndex(-1)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.label.setText(_translate("MainWindow", "Explore"))
-        self.pushButton_4.setText(_translate("MainWindow", "  Translate"))
-        self.pushButton_3.setText(_translate("MainWindow", "  Vocabulary"))
-        self.pushButton_2.setText(_translate("MainWindow", "  History"))
-        self.pushButton.setText(_translate("MainWindow", "  Favourite"))
+        self.pushButton_4.setText(_translate("MainWindow", "Translate"))
+        self.pushButton_3.setText(_translate("MainWindow", "Vocabulary"))
+        self.pushButton_2.setText(_translate("MainWindow", "History"))
+        self.pushButton.setText(_translate("MainWindow", "Favourite"))
 from static import resources_rc
