3d6b338d6e9152a332ffca29e4185715a5165a51
Ohm-Ittiphat <ikijpais@cmkl.ac.th>
2023-10-27 14:06:01 +0700

    Add pylint output

--- page_functions/history.py
+++ page_functions/history.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QWidget
-from ui.pages.History_page_ui import Ui_Form
+from ui.pages.HIstory_page_ui import Ui_Form
 class History(QWidget):
     def __init__(self):
         super(History, self).__init__()
